83e48a1952654c195ca39f9f924869414a0e3d4a
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-29 16:56:35 +0100

    dogfooding

--- run_typeguard.py
+++ run_typeguard.py
@@ -1,20 +1,26 @@
 import os
-from typeguard.util import TYPES_JSONL
-from typeguard.importhook import install_import_hook
-install_import_hook('dummy_module_2')
 
-from dummy_module_2 import dummy_fun, DummyClass2
-from package.dummy_module import DummyClass
+import numpy
+from typeguard.importhook import install_import_hook
+from dummy_package.dummy_module_2 import DummyClass2, dummy_fun
+
+TYPES_JSONL = "dummy_types.jsonl"
+os.environ["TYPES_JSONL"] = TYPES_JSONL
+install_import_hook('dummy_package')
+from dummy_package.dummy_module import DummyClass
+from dummy_package.another_dummy_module import AnotherDummyClass
 
 
 
 
 if __name__ == '__main__':
 
+
     if os.path.isfile(TYPES_JSONL):
         os.remove(TYPES_JSONL)
 
     x = DummyClass()
+    x.bla(numpy.zeros((1,3)))
     c = DummyClass2()
     y = dummy_fun(x)
 
@@ -22,6 +28,6 @@ if __name__ == '__main__':
     bla = c.dummy_method(x)
     bla = c.dummy_class_method(x)
     bla = c.dummy_static_method(x)
-    # print(x)
-    # print(y)
-    # print(bla)
+
+
+    x = AnotherDummyClass()
--- test_type_hinting.py
+++ test_type_hinting.py
@@ -7,26 +7,35 @@ from dummy_package.dummy_module import DummyClass
 from dummy_package.dummy_module_2 import DummyClass2, dummy_fun
 from redbaron_type_hinting.adding_type_hints import enrich_pyfiles_by_type_hints
 
+FILE_NAME = "types.jsonl"
 
 def test_type_hinting(tmp_path):
-    TYPES_JSONL = str(tmp_path / "types.jsonl")
+    TYPES_JSONL = str(tmp_path / FILE_NAME)
     os.environ["TYPES_JSONL"] = TYPES_JSONL
 
-    x = DummyClass()
-    x.bla(numpy.zeros((1,3)))
+    run_dummy()
 
+    enrich_pyfiles_by_type_hints(TYPES_JSONL)
+
+
+def run_dummy():
+    x = DummyClass()
+    x.bla(numpy.zeros((1, 3)))
     c = DummyClass2()
     y = dummy_fun(x)
-
     c.foo = x
     bla = c.dummy_method(x)
     bla = c.dummy_class_method(x)
     bla = c.dummy_static_method(x)
-
-
     x = AnotherDummyClass()
-    x.bla(numpy.zeros((1,3)))
+    x.bla(numpy.zeros((1, 3)))
+
+
+def test_dogfooding(tmp_path):
+    TYPES_JSONL = str(tmp_path / FILE_NAME)
+    os.environ["TYPES_JSONL"] = TYPES_JSONL
+
+    enrich_pyfiles_by_type_hints("dummy_types.jsonl")
 
     enrich_pyfiles_by_type_hints(TYPES_JSONL)
-
 
