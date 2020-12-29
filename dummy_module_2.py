6e88067bf04d0967811b59fca46cf2455429ed73
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-29 09:11:41 +0100

    dummy class 2

--- dummy_module_2.py
+++ dummy_module_2.py
@@ -7,6 +7,23 @@ def dummy_fun(x):
     x.foo = "dings"
     return x,"bar"
 
+class DummyClass2:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.foo = None
+
+
+    def dummy_method(self,x):
+        return x
+
+    @classmethod
+    def dummy_class_method(cls,x):
+        return x
+
+    @staticmethod
+    def dummy_static_method(x):
+        return x
 
 # def dummy_fun_annotated(x:DummyClass)->str:
 #     x.foo = "dings"
--- run_typeguard.py
+++ run_typeguard.py
@@ -2,7 +2,7 @@ import os
 from typeguard.importhook import install_import_hook
 install_import_hook('dummy_module_2')
 
-from dummy_module_2 import dummy_fun
+from dummy_module_2 import dummy_fun, DummyClass2
 from package.dummy_module import DummyClass
 
 
@@ -10,6 +10,13 @@ from package.dummy_module import DummyClass
 
 if __name__ == '__main__':
     x = DummyClass()
+    c = DummyClass2()
     y = dummy_fun(x)
-    print(x)
-    print(y)
+
+    c.foo = x
+    bla = c.dummy_method(x)
+    bla = c.dummy_class_method(x)
+    bla = c.dummy_static_method(x)
+    # print(x)
+    # print(y)
+    # print(bla)
