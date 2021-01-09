a90bd7a8392ace0b198b454df26e15dda3d98956
Tilo Himmelsbach <dertilo@gmail.com>
2021-01-09 20:41:16 +0100

    test_find_by_name_and_line_typegard

--- redbaron_type_hinting/test_finding_nodes.py
+++ redbaron_type_hinting/test_finding_nodes.py
@@ -1,8 +1,11 @@
+import os
+
 import pytest
 from baron.path import Position, BoundingBox
 from typing import List
 
 from redbaron import NameNode
+from typeguard.util import TYPEGUARD_CACHE, TypesLog
 
 from redbaron_type_hinting.util import read_red
 
@@ -38,8 +41,21 @@ def red():
 #     assert len(found_nodes) == 1
 #
 
-def test_find_by_name_and_line(red):
-    def_nodes:List[NameNode] = red.find_all("def", name="fun")
+# def test_find_by_name_and_line(red):
+#     def_nodes:List[NameNode] = red.find_all("def", name="fun")
+#     assert len(def_nodes) == 2
+#     assert def_nodes[0].absolute_bounding_box.top_left.line == 12 # decorator makes it start one line above
+#     assert def_nodes[1].absolute_bounding_box.top_left.line == 17
+
+
+def test_find_by_name_and_line_typegard(red):
+    foo = fun("bar")
+    file_to_typelog = {
+        f"{tl.func_module.replace('.','/')}.py": tl for tl in TYPEGUARD_CACHE.values()
+    }
+    type_log = file_to_typelog[f"{__file__.strip(os.getcwd())}"]
+
+    def_nodes: List[NameNode] = red.find_all("def", name="fun")
     assert len(def_nodes) == 2
-    assert def_nodes[0].absolute_bounding_box.top_left.line == 11 # decorator makes it start one line above
-    assert def_nodes[1].absolute_bounding_box.top_left.line == 16
+    assert type_log.qualname == "fun"
+    assert def_nodes[1].absolute_bounding_box.top_left.line == type_log.line
