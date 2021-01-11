004f3250ca86859ffb3557380499f8064fda37a0
Tilo Himmelsbach <dertilo@gmail.com>
2021-01-11 13:49:03 +0100

    test node capitalizing

--- redbaron_type_hinting/parse_seq_to_tree.py
+++ redbaron_type_hinting/parse_seq_to_tree.py
@@ -10,22 +10,24 @@ def dict_lists(mother, children=None):
     if children is not None:
         return {mother: list(children)}
     else:
-        assert False
+        return mother
 
 
-def parse_tree(seq: List[str], accum_siblings=dict_lists):
+def parse_tree(seq: List[str], accum_siblings=dict_lists, process_node=lambda x: x):
     while len(seq) > 0:
         node, stop_sign = eat_node(seq)
 
         if stop_sign == "[":
-            yield accum_siblings(node, parse_tree(seq, accum_siblings))
+            yield accum_siblings(
+                process_node(node), parse_tree(seq, accum_siblings, process_node)
+            )
         elif stop_sign == "]":
             if len(node) > 0:
-                yield node
+                yield process_node(node)
             break
         elif stop_sign == ",":
             if len(node) > 0:
-                yield node
+                yield process_node(node)
         else:
             assert False
 
@@ -39,10 +41,13 @@ def eat_node(seq):
     return node, x
 
 
-def to_string(mother: str, children: Generator[str, None, None]):
-    children = list(children)
-    assert all([isinstance(c, str) for c in children])
-    return f"{mother}[{','.join(children)}]"
+def to_string(mother: str, children: Generator[str, None, None] = None):
+    if children is not None:
+        children = list(children)
+        assert all([isinstance(c, str) for c in children])
+        return f"{mother}[{','.join(children)}]"
+    else:
+        return mother
 
 
 @pytest.mark.parametrize(
@@ -55,3 +60,17 @@ def to_string(mother: str, children: Generator[str, None, None]):
 )
 def test_tree_parsing(string):
     assert string == next(iter(parse_tree(list(string), to_string)))
+
+
+def capitalize_node(n):
+    return n.capitalize()
+
+
+@pytest.mark.parametrize(
+    "input,output",
+    [
+        ("a[b[cc[ddd]]]", "A[B[Cc[Ddd]]]"),
+    ],
+)
+def test_nod_capitalizing(input, output):
+    assert output == next(iter(parse_tree(list(input), to_string, capitalize_node)))
