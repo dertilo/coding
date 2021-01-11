ebe87fa4141c8e753eb184771ae90dba1e73af66
Tilo Himmelsbach <dertilo@gmail.com>
2021-01-11 13:35:53 +0100

    test parse_seq_to_tree.py

--- redbaron_type_hinting/parse_seq_to_tree.py
+++ redbaron_type_hinting/parse_seq_to_tree.py
@@ -1,17 +1,24 @@
 from pprint import pprint
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Generator
 
-Node = Union[Dict, str]
+import pytest
+
 STOP_SIGNS = ["[", ",", "]"]
 
 
-def parse_tree(seq: List[str]):
+def dict_lists(mother, children=None):
+    if children is not None:
+        return {mother: list(children)}
+    else:
+        assert False
+
+
+def parse_tree(seq: List[str], accum_siblings=dict_lists):
     while len(seq) > 0:
         node, stop_sign = eat_node(seq)
 
         if stop_sign == "[":
-            children = list(parse_tree(seq))
-            yield {node: children}
+            yield accum_siblings(node, parse_tree(seq, accum_siblings))
         elif stop_sign == "]":
             if len(node) > 0:
                 yield node
@@ -32,6 +39,19 @@ def eat_node(seq):
     return node, x
 
 
-if __name__ == "__main__":
-    pprint(list(parse_tree(list("Aa[Bbb,Cccc[Dd,Eee]]"))))
-    pprint(list(parse_tree(list("Aa[Bbb[Dd,Ee],Cccc]"))))
+def to_string(mother: str, children: Generator[str, None, None]):
+    children = list(children)
+    assert all([isinstance(c, str) for c in children])
+    return f"{mother}[{','.join(children)}]"
+
+
+@pytest.mark.parametrize(
+    "string",
+    [
+        "a[Bbb[Dd,Ee],xx[what],Cccc]",
+        "Aa[Bbb,Cccc[Dd,Eee]]",
+        "Aa[Bbb[Dd,Ee],Cccc]",
+    ],
+)
+def test_tree_parsing(string):
+    assert string == next(iter(parse_tree(list(string), to_string)))
