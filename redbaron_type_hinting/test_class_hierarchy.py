12ab1885ffb2c26f2055890c5701edbcd8cab68e
Tilo Himmelsbach <dertilo@gmail.com>
2021-01-12 11:29:08 +0100

    test_annotate_with_super_class

--- redbaron_type_hinting/test_class_hierarchy.py
+++ redbaron_type_hinting/test_class_hierarchy.py
@@ -31,22 +31,19 @@ def fun(x):
     return x
 
 
-foo = fun(Mother())
-foo = fun(Child())
+def test_annotate_with_super_class(red):
+    foo = fun(Mother())
+    foo = fun(Child())
 
-TYPES_LOGS = [
-    tl
-    for tl in TYPEGUARD_CACHE.values()
-    if f"{tl.func_module.replace('.','/')}.py" == __file__.strip(os.getcwd())
-    if tl.qualname.endswith("fun")
-]
-print(TYPES_LOGS)
-assert len(TYPES_LOGS) == 1
-
-
-@pytest.mark.parametrize(
-    "type_log", TYPES_LOGS, ids=[json.dumps(asdict(tl)) for tl in TYPES_LOGS]
-)
-def test_child(red, type_log):
+    TYPES_LOGS = [
+        tl
+        for tl in TYPEGUARD_CACHE.values()
+        if f"{tl.func_module.replace('.', '/')}.py" == __file__.strip(os.getcwd())
+        if tl.qualname.endswith("fun")
+    ]
+    assert len(TYPES_LOGS) == 1
+    type_log = TYPES_LOGS[0]
+    assert len(type_log.call_logs) == 2
     add_annotations(red, type_log)
-    pprint(red.dumps())
+    def_node = red.find("def", name="fun")
+    assert def_node.return_annotation.dumps() == "Mother"
