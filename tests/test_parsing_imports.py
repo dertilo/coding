cc235c05ca2d486065d4517a94761a488d483af9
Tilo Himmelsbach <dertilo@gmail.com>
2021-01-14 19:22:10 +0100

    get_target

--- tests/test_parsing_imports.py
+++ tests/test_parsing_imports.py
@@ -8,21 +8,20 @@ from redbaron import RedBaron
         ("import pytest", (None, "pytest", None)),
         ("import foo as bar", (None, "foo", "bar")),
         ("from foo import bar", ("foo", "bar", None)),
+        ("from foo.woo import bar", ("foo.woo", "bar", None)),
+        ("from foo.woo import bar as bazz", ("foo.woo", "bar", "bazz")),
     ],
 )
 def test_parse_imports(pysource, expected):
     parsed = (None, None, None)
     r = RedBaron(pysource)[0]
+    get_target = lambda x: x.target if len(x.target) > 0 else None
     if r.type == "import":
         value_root = r.value[0]
-        target = value_root.target
         value = value_root.value[0].value
-        if target == "":
-            parsed = (None, value, None)
-        else:
-            parsed = (None, value, target)
+        parsed = (None, value, get_target(value_root))
     elif r.type == "from_import":
         module = ".".join([x.value for x in r.value])
         value = r.targets[0].value
-        parsed = (module, value, None)
+        parsed = (module, value, get_target(r.targets[0]))
     assert expected == parsed
