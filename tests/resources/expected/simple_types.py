9b827f2b02900f1d8e080fbde4cdb08a365572e7
Tilo Himmelsbach <dertilo@gmail.com>
2021-01-15 10:34:12 +0100

    named_tuple_fun

--- tests/resources/expected/simple_types.py
+++ tests/resources/expected/simple_types.py
@@ -1,10 +1,22 @@
 from typing import List
+from typing import NamedTuple, Optional
+
+
 def str_fun(x:str)->str:
     return x
 
 def list_str_fun(x:List[str])->List[str]:
     return x
 
+class AudioConfig(NamedTuple):
+    format: str = "wav"
+    bitrate: Optional[int] = None
+    min_dur_secs: float = 0.5 # seconds
+
+def named_tuple_fun(x:AudioConfig)->AudioConfig:
+    return x
+
 def main():
     s = str_fun("foo")
     s = list_str_fun(["foo"])
+    _ = named_tuple_fun(AudioConfig())
--- tests/resources/test_cases/simple_types.py
+++ tests/resources/test_cases/simple_types.py
@@ -1,9 +1,21 @@
+from typing import NamedTuple, Optional
+
+
 def str_fun(x):
     return x
 
 def list_str_fun(x):
     return x
 
+class AudioConfig(NamedTuple):
+    format: str = "wav"
+    bitrate: Optional[int] = None
+    min_dur_secs: float = 0.5 # seconds
+
+def named_tuple_fun(x):
+    return x
+
 def main():
     s = str_fun("foo")
     s = list_str_fun(["foo"])
+    _ = named_tuple_fun(AudioConfig())
